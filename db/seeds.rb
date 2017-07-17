2a9c8349f04461ccc9fc57116bf67d6fb86fd308
Jun Ito <16290111jun@gmail.com>
2017-07-17 11:08:01 +0000

    Diver9 kadai

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,18 @@
-100.times do |n|
-  email = Faker::Internet.email
-  password = "password"
-  User.create!(email: email,
-               password: password,
-               password_confirmation: password,
-               )
+# 100.times do |n|
+#   email = Faker::Internet.email
+#   password = "password"
+#   User.create!(email: email,
+#               password: password,
+#               password_confirmation: password,
+#               )
+# end
+
+n = 1
+while n <= 100
+  Blog.create(
+    title: "あああ",
+    content: "hoge",
+    user_id: n
+  )
+  n = n + 1
 end
