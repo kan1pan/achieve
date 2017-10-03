423264aaf3caa1e5e452c2cac2854991f0654630
伊藤淳 <itojun@itojunMacBook-Pro.local>
2017-10-04 02:16:57 +0900

    Diver15 update_20171003

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,6 @@
 class CommentsController < ApplicationController
+  before_action :set_comment, only:[:edit, :update, :destroy]
+
   # コメントの保存・投稿アクション
   def create
     @comment = current_user.comments.build(comment_params)
@@ -6,7 +8,8 @@ class CommentsController < ApplicationController
 
     respond_to do |format|
       if @comment.save
-        format.html{ redirect_to blog_path(@blog), notice: 'コメントを投稿しました。'}
+        format.html{ redirect_to blog_path(@comment.blog), notice: 'コメントを投稿しました。'}
+        flash[:notice] = "コメントを投稿しました。"
         format.js { render :index }
       else
         format.html { render :new }
@@ -14,13 +17,23 @@ class CommentsController < ApplicationController
     end
   end
 
-  def destroy
-    @comment = Comment.find(params[:id])
+  def edit
     @blog = @comment.blog
+  end
 
+  def update
+    if @comment.update(comment_params)
+      redirect_to blog_path(@comment.blog), notice: 'コメントを編集しました。'
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
     respond_to do |format|
       if @comment.destroy
-        format.html{ redirect_to blog_path(@blog)}
+        format.html{ redirect_to blog_path(@comment.blog), notice: 'コメントを削除しました。'}
+        flash[:notice] = "コメントを削除しました。"
         format.js { render :index }
       end
     end
@@ -29,5 +42,9 @@ class CommentsController < ApplicationController
   private
     def comment_params
       params.require(:comment).permit(:blog_id, :content)
+    end
+
+    def set_comment
+      @comment = Comment.find(params[:id])
     end
 end
